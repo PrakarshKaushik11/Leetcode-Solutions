5970de9d0761f7f20960daea9d54f5ec6b033ab3
Prakarsh Kaushik <155573501+PrakarshKaushik11@users.noreply.github.com>
2026-08-26 10:00:31 +0530

    Time: 4 ms (59.05%), Space: 43 MB (64.63%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,15 +1,34 @@
-class Solution {
+// class Solution {
+//     public boolean halvesAreAlike(String s) {
+//         int hl = s.length() / 2;
+//         return vowelCount(s.substring(0, hl)) == vowelCount(s.substring(hl));
+//     }
+//     public int vowelCount(String s) {
+//         int cnt = 0;
+//         for (char c : s.toCharArray()) {
+//             if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+//                 cnt++;
+//             }
+//         }
+//         return cnt;
+//     }
+// }
+class Solution{
     public boolean halvesAreAlike(String s) {
-        int hl = s.length() / 2;
-        return vowelCount(s.substring(0, hl)) == vowelCount(s.substring(hl));
-    }
-    public int vowelCount(String s) {
-        int cnt = 0;
-        for (char c : s.toCharArray()) {
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
-                cnt++;
+        int mid=s.length()/2;
+        int cnt1=0;
+        int cnt2=0;
+        String vowels = "aeiouAEIOU";
+        for (int i=0;i<mid;i++) {
+            if (vowels.indexOf(s.charAt(i))!=-1) {
+                cnt1++;
             }
         }
-        return cnt;
+        for (int i=mid;i<s.length();i++) {
+            if (vowels.indexOf(s.charAt(i))!=-1) {
+                cnt2++;
+            }
+        }
+        return cnt1==cnt2;
     }
 }
