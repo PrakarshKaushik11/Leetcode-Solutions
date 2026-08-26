cb738fd69eeb36a47625c6c1d83a8095376a1424
Prakarsh Kaushik <155573501+PrakarshKaushik11@users.noreply.github.com>
2026-08-26 09:51:19 +0530

    Time: 0 ms (100%), Space: 43 MB (64.63%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,18 +1,25 @@
 class Solution {
-    public boolean halvesAreAlike(String s) {
-        int n = s.length();
-        int count = 0;
-        for (int i = 0; i < n / 2; i++) {
-            if (isVowel(s.charAt(i)))
-            count++;
+    static{
+        for(int i =0;i<500;i++){
+            halvesAreAlike("");
         }
-        for (int i=n/2;i< n; i++) {
-            if (isVowel(s.charAt(i)))
-            count--;
-        }
-        return count == 0;
     }
-    boolean isVowel(char c) {
-        return "aeiouAEIOU".indexOf(c) != -1;
+    public static boolean halvesAreAlike(String s) {
+        int mid=0+((s.length()-1)-0)/2;
+        int i=0,j=mid+1;
+        int count1=0,count2=0;
+        while(i<=mid&&j<s.length()){
+          char ch1=s.charAt(i);
+          char ch2=s.charAt(j);
+          if(ch1=='A'||ch1=='a'||ch1=='E'||ch1=='e'||ch1=='I'||ch1=='i'||ch1=='O'||ch1=='o'||ch1=='U'||ch1=='u'){
+            count1++;
+          }
+          if(ch2=='A'||ch2=='a'||ch2=='E'||ch2=='e'||ch2=='I'||ch2=='i'||ch2=='O'||ch2=='o'||ch2=='U'||ch2=='u'){
+            count2++;
+          }
+          i++;
+          j++;
+        }
+        return (count1==count2);
     }
 }
