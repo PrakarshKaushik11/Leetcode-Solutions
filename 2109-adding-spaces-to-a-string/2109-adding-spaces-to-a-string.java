de90542962a8b089b322c0e7074484ad96517a8f
Prakarsh Kaushik <155573501+PrakarshKaushik11@users.noreply.github.com>
2026-08-26 10:22:01 +0530

    Time: 21 ms (80.84%), Space: 90 MB (67.38%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.java
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.java
@@ -1,21 +1,36 @@
+// class Solution {
+//     public String addSpaces(String s, int[] spaces) {
+//         int n=s.length();
+//         int m=spaces.length;
+//         char[] output = new char[n+m];
+//         int space=0;
+//         int ind=0;
+//         int j=0;
+//         for(int i=0;i<n+m;i++){
+//             if(space<m && spaces[space]==j){
+//                 output[i] = ' ';
+//                 space++;
+//             }
+//             else{
+//                 output[i] = s.charAt(j);
+//                 j++;
+//             } 
+//         }
+// 		return new String(output);
+//     }
+// }
+
 class Solution {
     public String addSpaces(String s, int[] spaces) {
-        int n=s.length();
-        int m=spaces.length;
-        char[] output = new char[n+m];
-        int space=0;
-        int ind=0;
-        int j=0;
-        for(int i=0;i<n+m;i++){
-            if(space<m && spaces[space]==j){
-                output[i] = ' ';
-                space++;
-            }
-            else{
-                output[i] = s.charAt(j);
-                j++;
-            } 
+    StringBuilder r = new StringBuilder();
+    int j=0 ; 
+    for(int i=0;i<s.length();i++){
+        if(j<spaces.length && i==spaces[j]){
+            r.append(' ');
+            j++;
         }
-		return new String (output);
+        r.append(s.charAt(i));
+    }
+        return r.toString();
     }
 }
